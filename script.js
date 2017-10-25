1c1f629c51b963f64f62d918430bcdfefe25b4ce
Beckett Jenen <beckettjenen@gmail.com>
2017-10-25 08:26:06 -0400

    JIC

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+ var pullfiles=function(){ 
+    // love the query selector
+    var fileInput = document.querySelector("#file-to-upload");
+    var files = fileInput.files;
+    // cache files.length 
+    var fl=files.length;
+    var i=0;
+
+    while ( i < fl) {
+        // localize file var in the loop
+        var file = files[i];
+        alert(file.name);
+        i++;
+    }    
+}
+
+
 function email() {
     var nodemailer = require('nodemailer');
     var transporter = nodemailer.createTransport({
@@ -28,7 +45,7 @@ function email() {
     
     //THIS IS THE PART WHERE YOU CHANGE HOW EVERY MESSAGE SENDS
     mailOptions.to = $("#to").val();
-    mailOptions.html = '<h1 style="text-decoration: none; color: #00743F;"> This information trade request was sent by ' + identifierTwo + '</h1> <br> <a href="Users/beckettjenen/Desktop/electron/download.html" style="background-color: #00743F; color: white; border: 1px solid transparent; outline: none; padding-top: 15px; padding-bottom: 15px; padding-left: 20px; padding-right: 20px; font-size: 25px; border-radius: 50px; -ms-touch-action: manipulation; touch-action: manipulation; cursor: pointer; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; margin-right: 1%;">Accept</a> <a href="#" style="background-color: #00743F; color: white; border: 1px solid transparent; outline: none; padding-top: 15px; padding-bottom: 15px; padding-left: 20px; padding-right: 20px; font-size: 25px; border-radius: 50px; -ms-touch-action: manipulation; touch-action: manipulation; cursor: pointer; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none;">Add More</a> <br> <h1 style="text-decoration: none; color: #00743F;"> Information to be traded </h1> <h2 style="color: black; text-decoration: none; -webkit-text-security: disc;">' + forge_sha256(identifier) + '</h2> <br> <h1 style="text-decoration: none; color: #00743F;"> Additional Information: </h1> <h2 style="color: black; text-decoration: none; -webkit-text-security: disc;">' + identifierThree + '</h2>';
+    mailOptions.html = '<h1 style="text-decoration: none; color: #00743F;"> This information trade request was sent by ' + identifierTwo + '</h1> <br> <a href="file:///Users/beckettjenen/Desktop/electron/addinfo.html" style="background-color: #00743F; color: white; border: 1px solid transparent; outline: none; padding-top: 15px; padding-bottom: 15px; padding-left: 20px; padding-right: 20px; font-size: 25px; border-radius: 50px; -ms-touch-action: manipulation; touch-action: manipulation; cursor: pointer; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; margin-right: 1%;">Accept</a> <a href="#" style="background-color: #00743F; color: white; border: 1px solid transparent; outline: none; padding-top: 15px; padding-bottom: 15px; padding-left: 20px; padding-right: 20px; font-size: 25px; border-radius: 50px; -ms-touch-action: manipulation; touch-action: manipulation; cursor: pointer; -webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none;">Add More</a> <br> <h1 style="text-decoration: none; color: #00743F;"> Information to be traded </h1> <h2 style="color: black; text-decoration: none; -webkit-text-security: disc;">' + forge_sha256(identifier) + '</h2> <br> <h1 style="text-decoration: none; color: #00743F;"> Additional Information: </h1> <h2 style="color: black; text-decoration: none; -webkit-text-security: disc;">' + identifierThree + '</h2>';
     
 
     transporter.sendMail(mailOptions, function(error, info){
